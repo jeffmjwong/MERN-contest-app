9a4da35eb490bd819338a333e117082a83a9f0e5
Jeff Wong <jeffmjwong@gmail.com>
2018-06-13 21:05:52 +1000

    Use EJS for templating html page and move header and footer as partial

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-import config, { nodeEnv, logStars } from './config';
+import express from 'express';
+import config from './config';
+import apiRouter from './api';
 
-logStars('hehehe');
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (request, response) => {
+  response.render('index', {
+    content: 'Hello Express and <em>EJS</em>!'
+  });
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}...`);
+});
